8b61fc914b4dd736c4917a2d61014ec4bd8934d1
Michal Nánási <michal.nanasi@gmail.com>
2012-11-09 15:31:08 +0100

    Added memory backward algorithm

--- PairHMM.py
+++ PairHMM.py
@@ -1,6 +1,5 @@
 import MemoryPatterns
 from collections import defaultdict
-from itertools import product
 
 class State:
     def __init__(self):
@@ -38,13 +37,15 @@ class RepeatState(State):
         
     def emission(self, X, Y, x, y, dx, dy):
         self.hmm.getProbability(X, Y, x, y, dx, dy)
-        # vyrobime HMM, vypocitame hodnotu a zacachujeme ju, lebo ju budeme mozno neskor znova potrebovat
-        #TODO: pridaj aj hashovanie a memoizaciu, nech nekonstruujem tie iste objekty viac krat -- na to by sa mozno hodila nejaka factory       
+        # vyrobime HMM, vypocitame hodnotu a zacachujeme ju, lebo ju budeme 
+        # mozno neskor znova potrebovat
+        # TODO: pridaj aj hashovanie a memoizaciu, nech nekonstruujem tie iste 
+        # objekty viac krat -- na to by sa mozno hodila nejaka factory       
         
 
 class GeneralizedPairHMM:
     
-    #TODO: este pridat dalsie restrikcie z anotacie
+    # TODO: este pridat dalsie restrikcie z anotacie
     
     states = list()
     transitions = dict()
@@ -53,7 +54,7 @@ class GeneralizedPairHMM:
         self.transitions.setdefault(dict());
         return
     
-    #Pridame stav, vrati sa nam jeho idcko
+    # Pridame stav, vrati sa nam jeho idcko
     def addState(self, state):
         state.setID(len(list))
         list.append(state)
@@ -63,7 +64,7 @@ class GeneralizedPairHMM:
         self.transitions[stateFrom][stateTo] = probability
         
     def optimize(self):
-        #vyrobi potrebne tabulky, aby sme vedeli rychlo pocitat
+        # Vyrobi potrebne tabulky, aby sme vedeli rychlo pocitat
         self.__transitions = list()
         self.__reverse_transitions = list()
         for _ in range(len(self.states)):
@@ -77,7 +78,7 @@ class GeneralizedPairHMM:
     # Returns forward table. We can specify memory pattern, position generator
     # and initial row. Compatible with memory preserving tricks 
     # TODO: +-1
-    # TODO: allow restrictions
+    # TODO: allow restrictions 
     def getForwardTable(self, X, Y, x, y, dx, dy,
         memoryPattern = None, positionGenerator = None, initialRow = None):
         # Default position generator
@@ -91,16 +92,16 @@ class GeneralizedPairHMM:
             return
         
         # Initialize table
-        rows = [defaultdict(lambda *_:defaultdict(int)) for _ in range(dx+1)]
+        rows = [defaultdict(lambda *_:defaultdict(float)) for _ in range(dx+1)]
         
         # Initialize first row
         ignoreFirstRow = False
         if initialRow != None:
             rows[0] = initialRow
-            ignoreFirstRow = True;
+            ignoreFirstRow = True
         else:
             for state in self.states:
-                rows[0][0][(state.getStateID(),0,0)] = \
+                rows[0][0][(state.getStateID(), 0, 0)] = \
                     state.getStartProbability()
         
         # Main algorithm
@@ -126,18 +127,86 @@ class GeneralizedPairHMM:
             # If rows were changed, remember it
             if _x_prev != _x:
                 if memoryPattern.next():
-                    retTable.append((x + _x, rows[_x]))
-                rows[_x] = list()
+                    retTable.append((x + _x, rows[_x_prev]))
+                rows[_x_prev] = list()
             _x_prev = _x
         
         # Remember last row if necessary
         if memoryPattern.next():
-            retTable.append((x + _x_prev, rows[_x]))   
+            retTable.append((x + _x_prev, rows[dx]))   
         
         # Finally, done:-)
         return retTable
     
-    def getBackwardTable(self, X, Y, x, y, dx, dy):
+    # Basically copy of the getForwardTable. Might share bugs
+    # TODO: +- 1
+    # TODO: restrictions
+    # TODO: reverse memory pattern?
+    def getBackwardTable(self, X, Y, x, y, dx, dy,
+        memoryPattern = None, positionGenerator = None, initialRow = None):
+        # Default position generator
+        if positionGenerator == None:
+            positionGenerator = \
+                ((i,j) for i in range(dx + 1) for j in range(dy + 1))
+        # Some generators have to be reversed
+        positionGenerator = reversed(list(positionGenerator))
+        
+        # Default memory pattern (everything)
+        if memoryPattern == None:
+            memoryPattern = MemoryPatterns.every(dx)
+        
+        # Initialize table
+        rows = [defaultdict(lambda *_:defaultdict(float)) for _ in range(dx+1)]
+        
+        # Initialize first row
+        ignoreFirstRow = False
+        if initialRow != None:
+            rows[dx] = initialRow
+            ignoreFirstRow = True
+        else:
+            for state in self.states:
+                rows[dx][dy][state.getStateID(), 0, 0] = \
+                    state.getEndProbability()
+        
+        # Main algorithm
+        _x_prev = x + dx + 100000
+        retTable = list()
+        for (_x, _y) in positionGenerator:
+            if ignoreFirstRow and _x == dx:
+                continue
+            for (stateID, _dx, _dy) in rows[x + _dx][y + _dy].iteritems():
+                state = self.states[stateID]
+                for previousID in state.previousIDs():
+                    previous = self.states[previousID]
+                    for (_sdx, _sdy) in previous.durationGenerator():
+                        rows[_x - _sdx][_y - _sdy][(previous, _sdx, _sdy)] = \
+                            previous.emission(
+                                X,
+                                Y,
+                                x + _x,
+                                x + _y,
+                                _dx,
+                                _dy
+                            ) * self.transitions[previousID][stateID]
+            
+            # Remember last row if necessary
+            if _x_prev != _x:
+                if memoryPattern.next():
+                    retTable.append((x + _x, rows[_x_prev]))
+                rows[_x_prev] = list()  
+            _x_prev = _x
+        
+        # Remember last row if necessary
+        if memoryPattern.next():
+            retTable.append((x + _x_prev, rows[0]))
+                             
+        # Finally, done:-)
+        return retTable
+    
+    def getViterbiTable(self, X, Y, x, y, dx, dy):
+        return
+    
+    def getViterbiAlignment(self, table):
         return
     
     def getProbability(self, X, Y, x, y, dx, dy, positionGenerator):
