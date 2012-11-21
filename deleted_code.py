0e5f0314d42254c1a2612ba47114935aed5a6ae7
Michal Nánási <michal.nanasi@gmail.com>
2012-11-21 18:31:01 +0100

    +Deleted code

---
+++ deleted_code.py
@@ -0,0 +1,45 @@
+    def load(self, dictionary):
+        HMM.load(self, dictionary)
+        a = GeneralizedState(self.mathType)
+        a.load({
+            "__name__" : "GeneralizedState",
+            "durations" : [(0, 1.0)],
+            "name" : "__InitState__",
+            "emission" : [("", 0.0)],
+            "startprob" : self.mathType(1.0),
+            "endprob" : self.mathType(0.0),
+            "serialize" : False
+        });
+        ID = self.addState(a)
+        for stateID in range(len(self.states)):
+            if ID == stateID: 
+                continue
+            prob = self.states[stateID].getStartProbability()
+            if prob > self.mathType(0.0):
+                self.addTransition(ID, stateID, prob)
+        a = GeneralizedState(self.mathType)
+        a.load({
+            "__name__" : "GeneralizedState",
+            "durations" : [(0, 1.0)],
+            "name" : "__EndState__",
+            "emission" : [("", 0.0)],
+            "startprob" : self.mathType(0.0),
+            "endprob" : self.mathType(1.0),
+            "serialize" : False
+        })
+        ID = self.addState(a)
+        for stateID in range(len(self.states)):
+            if ID == stateID: 
+                continue
+            prob = self.states[stateID].getEndProbability()
+            if prob > self.mathType(0.0):
+                self.addTransition(stateID, ID, prob)
+
+
+       
+            ret = X
+            print(ret)
+            print("[")
+            for (_, lst) in ret:
+                print("["+", ".join(["{"+ ", ".join([str(xxx)+": "+str(vvv) for (xxx,vvv) in xx.iteritems()])+"}" for xx in lst]) + "],")
+            print("]")
