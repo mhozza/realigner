87b3b26e33ea5f5975a9e9bc20b118c0056ad6ac
Michal Nánási <michal.nanasi@gmail.com>
2012-12-18 12:07:18 +0100

    + Added option to dynamically load dictionaries into objectHook while loading same file

--- tools/ConfigFactory.py
+++ tools/ConfigFactory.py
@@ -32,7 +32,9 @@ class ConfigFactory:
     def addConstant(self, name, constant):
         self.constants[name] = constant
         
-    def addDictionary(self, name, dictionary):
+    def addDictionary(self, name, dictionary, check_if_exists=False):
+        if check_if_exists and name in self.dictionary:
+            return
         self.dictionary[name] = dictionary
 
     def objectHook(self, dictionary):
@@ -40,10 +42,26 @@ class ConfigFactory:
         Object has method load. If it returns None, then the instance of 
         the object will be replaced with a dictionary. If load returns value X
         that is not none, dictionary will be replaced with X.
+        
+        It is possible to dynamically add dictionaries. If __name__ start with @, it means that
+        it will be added as dictionary into objectHook. If it ends with ?, it will be added only
+        if it is not there already
         """
+        
         if "__name__" not in dictionary:
             return dictionary
         cn = dictionary["__name__"]
+        if len(cn) > 0 and cn[0] == '@':
+            if cn[-1] == "?":
+                cnn = cn[1:-1]
+                check_if_exists = True
+            else:
+                cnn = cn[1:]
+                check_if_exists = False
+            del dictionary['__name__']
+            self.addDictionary(cnn, dictionary, check_if_exists)
+            dictionary['__name__'] = cn
+            return None
         if cn in self.objects:
             obj = self.objects[cn]()
             ret = obj.load(dictionary)
@@ -62,7 +80,7 @@ class ConfigFactory:
         elif cn in self.constants:
             return self.constants[cn]
         else:
-            return dictionary
+            return None
 
 
     def load(self, filename):
