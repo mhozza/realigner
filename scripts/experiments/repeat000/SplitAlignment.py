fee60cfcf9a878a89fbd6c27380a80941eccab0d
Michal Nánási <michal.nanasi@gmail.com>
2013-02-21 15:13:16 +0100

    * Finished

--- scripts/experiments/repeat000/SplitAlignment.py
+++ scripts/experiments/repeat000/SplitAlignment.py
@@ -1,10 +1,9 @@
 import json
 import os
-import subprocess
 import argparse
 from bin.Maf2Fasta import Maf2FastaGen
 
-def main(alignment, output_file, working_directory, split_count, paralelism=20):
+def main(alignment, working_directory, split_count=40, output_file):
     
     # TODO: check na to, ci to uz existuje, a ked ano, tak to nespravil znova
     
@@ -19,7 +18,6 @@ def main(alignment, output_file, working_directory, split_count, paralelism=20):
         # fasta_generator = alignment
         assert(False)
     elif extension == 'maf':
-        file = '{dir}/{base}.fa'.format(dir=working_directory, base=base)
         fasta_generator = Maf2FastaGen(alignment)
     else:
         assert(False)
@@ -32,21 +30,28 @@ def main(alignment, output_file, working_directory, split_count, paralelism=20):
     if not os.path.exists(parallel_dir):
         os.makedirs(parallel_dir)
     
-    files = [open()]
+    files = [open('alignment_{0:04d}.fa'.format(i), 'w') 
+             for i in range(split_count)]
     
     for src, aln_count, text in fasta_generator:
-        files[aln_count % paralelism].write(
+        files[aln_count % split_count].write(
             '>{0}.{1}\n{2}\n'.format(src, aln_count, text)
         )
+        
     map(lambda x: x.close(), files)
+    
+    with open(output_file, 'w') as f:
+        json.dump(files, f, indent=4)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Prepare parameters')
     parser.add_argument('alignment', type=str, help='Input alignment')
-    parser.add_argument('output', type=str, help='Output file')
-    parser.add_argument('workink_dir', type=str, help='Working directory')
+    parser.add_argument('working_dir', type=str, help='Working directory')
+    parser.add_argument('split_count', type=str,
+                        help='To how many alignments split')
+    parser.add_argument('output', type=str,
+                        help='Output file containing generated files')
+    
     parsed_arg = parser.parse_args()
-    main()
-
-
+    main(parsed_arg.alignment, parsed_arg.working_dir, parsed_arg.output)
