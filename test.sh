72b57947d02dcd0dbd196983db535c06e1e40b6c
Michal Nánási <mic@git.ksp.sk>
2013-03-27 08:53:01 +0100

    * Updated version of testing script

--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
-#!/Bin/bash
+#!/bin/bash
+
+if [ `uname -n` == 'panda-cub' ]; then 
+        echo -e '\e[1;31mNa notebooku sa nespustim!'
+        tput sgr0
+        exit 1
+fi
 
 export PYTHONPATH=./ #\:/usr/lib/python2.7/dist-packages
 PYTHON=pypy-env/bin/pypy
@@ -15,7 +21,8 @@ case 'nosample' in
 		output_file_from_split.txt \
 		'hg19.*' \
 		'canFam2.*'
-	
+
+
 # Compute statistics from input alignment
 	time $PYTHON scripts/experiments/repeat000/PrepareParameters.py \
 		output_file_from_split.txt \
@@ -33,7 +40,8 @@ case 'nosample' in
 		output_file_from_prepare.*.txt \
 		working_dir_tmp/stat \
 		output_file_from_aggregate.txt
-	
+
+
 # Create model
 	time $PYTHON scripts/experiments/repeat000/CreateModel.py \
 		data/models/repeatHMM.js \
@@ -44,19 +52,23 @@ case 'nosample' in
 # Sample alignments
 	time $PYTHON bin/Sample.py \
 		working_dir_tmp/sampled_alignments/{id}.fa \
-		1 \
+		10 \
 		1000 \
 		1000 \
 		--output_files output_from_sample.txt \
 		--model output_file_from_create.txt
-	;&
+
+		;&
 *)	
+
 
 	export SGE_TASK_FIRST=1
 	export SGE_TASK_ID=1
 	export SGE_TASK_LAST=10
 	export SGE_STEP_SIZE=1
-	
+
+	./track.sh > memory_profile.csv 2> /dev/null &
+
 	time $PYTHON bin/Realign.py \
 		working_dir_tmp/sampled_alignments/{id}.fa \
 		working_dir_tmp/sampled_alignments/{id}.realigned.fa \
@@ -65,9 +77,13 @@ case 'nosample' in
 		--beam_width 30 \
 		--repeat_width 0 \
 		--sequence_regexp sequence1 sequence2 \
-		--tracks original_repeats  \
+		--tracks original_repeats,trf  \
+		--draw output.png #\
 		--intermediate_output_files 'posterior:table.js,viterbi:viterbi.js,viterbi_path:viterbi_path.js' #\
 		#--intermediate_input_files 'posterior:table.js,viterbi:viterbi.js,viterbi_path:viterbi_path.js' 
-				
+	
+	killall -9 track.sh
+	wait
+			
 	;&
 esac
