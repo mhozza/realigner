0aafd6a73d054662d1ec4ef68e56cec58cfb2a15
Michal Hozza <mhozza@gmail.com>
2013-03-31 13:56:29 +0200

    wrapper pre klasifikator + pomocne metody na nacitanie dat, trenovanie... este to nie je hotove

--- hack/PairClassifier.py
+++ hack/PairClassifier.py
@@ -3,9 +3,12 @@ Created on Mar 28, 2013
 
 @author: Michal Hozza
 '''
+from alignment import Fasta, Alignment
 from copy import deepcopy
-import pickle
+from os import path, listdir
 from sklearn.ensemble import RandomForestClassifier
+import pickle
+import sys
 #from numpy import mean
 
 
@@ -43,17 +46,17 @@ class AnnotatedBaseCouple:
 
 
 class PairClassifier:
-	def __init__(self):
-		self.classifier = RandomForestClassifier(n_estimators=10, n_jobs=4)		
+    def __init__(self):
+        self.classifier = RandomForestClassifier(n_estimators=10, n_jobs=4)
 
-    def load(self, file):
-    	self.classifier = pickle.load(file)
-    
-    def save(self, file):
-    	pickle.dump(self.classifier, file)
+    def load(self, f):
+        self.classifier = pickle.load(f)
+
+    def save(self, f):
+        pickle.dump(self.classifier, f)
 
     def fit(self, data, target):
-    	self.classifier.fit(data, target)
+        self.classifier.fit(data, target)
 
     def predict(self, data):
 #    	return self.classifier.predict(data)
@@ -62,6 +65,61 @@ class PairClassifier:
         return self.classifier.predict_proba(data)
 
 
+class DataLoader:
+#    def __init__(self):
 
 
-    
+    def _SequencetoAnnotatedBaseCoupleList(self,seqX, annotationsX,
+                                           seqY, annotationsY):
+        pass
+
+    def _getAnnotations(self, fname):
+        pass
+
+    def _getSequencesAlignment(self, fname):
+        alignment_regexp = ""
+        sequence_regexp = "alignment"
+        #pre zaciatok berieme len prve zarovnanie
+        aln = Fasta.load(fname, alignment_regexp, Alignment, sequence_regexp )[0]
+        if len(aln.sequences) < 2:
+            sys.stderr.write("ERROR: not enough sequences in file\n")
+            exit(1)
+
+#        original_annotation = aln.sequences[2]
+        aln.popSequence()
+
+        # Sequence 1
+        seq1 = Fasta.alnToSeq(aln.sequences[0])
+#        seq1_length = len(seq1)
+#        seq1_name = aln.names[0]
+
+        # Sequence 2
+        seq2 = Fasta.alnToSeq(aln.sequences[1])
+#        seq2_length = len(seq2)
+#        seq2_name = aln.names[1]
+
+        return (seq1, seq2)
+
+
+    def loadSequence(self, fname):
+        #constants
+        annotationsSubdirName = "annotations"
+
+        base = path.basename(fname)
+        directory = path.dirname(fname)
+        annotationsFname = path.join(directory,annotationsSubdirName,base)
+
+        seqX, seqY = self._getSequencesAlignment(fname)
+        annotationsX, annotationsY = self._getAnnotations(annotationsFname)
+
+        return self._SequencetoAnnotatedBaseCoupleList(seqX, annotationsX, seqY,
+                                                       annotationsY)
+
+
+    def loadDirectory(self, dirname):
+        sequences = []
+        for fname in listdir(dirname):
+            if path.isfile(fname):
+                sequences.append(self.loadSequence(fname))
+        return sequences
+
