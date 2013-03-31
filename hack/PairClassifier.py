1d8ce6017fe955962c14d44a5b6fb4443e727813
Michal Hozza <mhozza@gmail.com>
2013-03-31 18:41:37 +0200

    Priprava trenovacich dat

--- hack/PairClassifier.py
+++ hack/PairClassifier.py
@@ -6,20 +6,32 @@ Created on Mar 28, 2013
 from alignment import Fasta
 from alignment.Alignment import Alignment
 from copy import deepcopy
+from numpy import array
 from os import path, listdir
 from sklearn.ensemble import RandomForestClassifier
+import itertools
 import pickle
 import sys
 
 
 class AnnotatedBase:
     def __init__(self):
-        self.base = -1
+        self.base = '-'
         self.annotations = {}
 
     def copy(self, ab):
         self.base = ab.base
         self.annotations = deepcopy(ab.annotations)
+
+    def toTrainData(self, last):
+        m = {'A':0, 'C':1, 'G':2, 'T':3, '-':-1}
+
+        if(self.base!='-'):
+            last.copy(self)
+        res = [m[last.base]]
+        for i in self.annotations:
+            res.append(i)
+        return res
 
 
 class AnnotatedBaseCouple:
@@ -28,21 +40,11 @@ class AnnotatedBaseCouple:
         self.X = AnnotatedBase()
         self.Y = AnnotatedBase()
 
-    def toArray(self):
-        a = [self.X.base, self.Y.base]
+    def toTrainData(self, lastX, lastY):
+        return array(list(itertools.chain(*zip(self.X.toTrainData(lastX), self.Y.toTrainData(lastY)))))
 
-        for annotation in self.annotations:
-            try:
-                a.append(self.X.annotations[annotation])
-            except KeyError, e:
-                a.append(-1)
-
-            try:
-                a.append(self.Y.annotations[annotation])
-            except KeyError, e:
-                a.append(-1)
-        return a
-
+    def isAligned(self):
+        return self.X.base != '-' != self.Y.base
 
 
 class PairClassifier:
@@ -66,12 +68,26 @@ class PairClassifier:
 
 
 class DataLoader:
-#    def __init__(self):
-
-
-    def _SequencetoAnnotatedBaseCoupleList(self,seqX, annotationsX,
+    def _SequenceToAnnotatedBaseCoupleList(self, annotations, seqX, annotationsX,
                                            seqY, annotationsY):
-        pass
+        if len(seqX) != len(seqY):
+            print seqX
+            print seqY
+            sys.stderr.write("ERROR: sequences does not have same length\n")
+            exit(1)
+
+        data = list()
+
+        for i in range(len(seqX)):
+            b = AnnotatedBaseCouple(annotations)
+            b.X.base = seqX[i]
+#            b.X.annotations = annotationsX[:,i]
+            b.Y.base = seqY[i]
+#            b.Y.annotations = annotationsY[:,i]
+            data.append(b)
+
+        return data
+
 
     def _getAnnotations(self, fname):
         return (None,None)
@@ -85,19 +101,15 @@ class DataLoader:
         if aln==None or len(aln.sequences) < 2:
             sys.stderr.write("ERROR: not enough sequences in file\n")
             exit(1)
-
-        # Sequence 1
         seq1 = aln.sequences[0]
-
-        # Sequence 2
         seq2 = aln.sequences[1]
-
         return (seq1, seq2)
 
 
     def loadSequence(self, fname):
         #constants
         annotationsSubdirName = "annotations"
+        annotations = ["Gene"]
 
         base = path.basename(fname)
         directory = path.dirname(fname)
@@ -105,10 +117,18 @@ class DataLoader:
 
         seqX, seqY = self._getSequencesAlignment(fname)
         annotationsX, annotationsY = self._getAnnotations(annotationsFname)
+        return self._SequenceToAnnotatedBaseCoupleList(annotations,
+                                                       seqX, annotationsX,
+                                                       seqY, annotationsY)
 
-        return self._SequencetoAnnotatedBaseCoupleList(seqX, annotationsX, seqY,
-                                                       annotationsY)
-
+    def prepareTrainingData(self, abcList):
+        train_data = (list(), list())
+        #TODO - vyriesit pripad ked niektora sekvencia zacina -
+        lastX, lastY = (AnnotatedBase(), AnnotatedBase())
+        for i in abcList:
+            train_data[0].append(i.toTrainData(lastX, lastY))
+            train_data[1].append(i.isAligned())
+        return train_data
 
     def loadDirectory(self, dirname):
         sequences = []
@@ -125,5 +145,8 @@ if __name__ == "__main__":
         exit(1)
 
     d = DataLoader()
-    d.loadSequence(fname)
-    print (d._getSequencesAlignment(fname))
+    seq = d.loadSequence(fname)
+    data, target = d.prepareTrainingData(seq)
+    for i,j,s in zip(data, target, seq):
+        print i, s.X.base, s.Y.base, j
+    # print (d._getSequencesAlignment(fname))
