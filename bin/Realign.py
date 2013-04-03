31dfe028180b18d50f4dedce8fc4f9012dc41563
Michal Nánási <michal.nanasi@gmail.com>
2013-04-03 14:01:59 +0200

    * Refactored Realigner (main function was separated into several smaller functions.

--- bin/Realign.py
+++ bin/Realign.py
@@ -18,7 +18,10 @@ from alignment.ViterbiRealigner import ViterbiRealigner
 def brainwash(className):
     dct = dict();
     for key, value in className.__dict__.iteritems():
-        dct[key] = lambda *_, **__: None
+        if hasattr(value, '__call__'):
+            dct[key] = lambda *_, **__: None
+        else: 
+            dct[key] = None
     return type('Brainwashed' + className.__name__, (object,), dct)
 
 
@@ -32,6 +35,49 @@ def alignment_column_to_annotation(column):
         return 'M'
     else:
         return '-'
+
+
+def get_model(args):
+    loader = HMMLoader(args.mathType) # TODO: rename HMMLoader to ModelLoader
+    for i in range(0, len(args.bind_constant), 2):
+        loader.addFile(args.bind_file[i], args.bind_file[i + 1])
+    for i in range(0, len(args.bind_constant_file), 2):
+        loader.addConstant(
+            args.bind_constant_file[i],
+            loader.load(args.bind_constant_file[i + 1])
+        )
+    for i in range(0, len(args.bind_constant), 2):
+        loader.addConstant(
+            args.bind_constant[i],
+            loader.loads(args.bind_constant[i + 1]),
+        )
+    return loader.load(args.model)["model"]
+
+
+def compute_annotations(args, alignment_filename):
+    annotations = dict()
+    if 'trf' in args.tracks:
+        trf = None
+        for trf_executable in args.trf:
+            if os.path.exists(trf_executable):
+                trf = TRFDriver(trf_executable, mathType=args.mathType)
+                #break
+        if trf:
+            repeats = trf.run(alignment_filename)
+            annotations['trf'] = repeats
+                        
+    if 'original_repeats' in args.tracks:
+        repeats = json.load(Open(alignment_filename + '.repeats',
+                                 'r'))
+        for k, v in repeats.iteritems():
+            repeats[k] = [Repeat(_v[0], _v[1], _v[2], _v[3], _v[4]) 
+                          for _v in v]
+        
+        annotations['original_repeats'] = repeats
+            
+    perf.msg("Hints computed in {time} seconds.")
+    perf.replace()
+    return annotations
 
     
 def getMathType(s):
@@ -55,10 +101,8 @@ def getRealigner(s):
     else:
         raise('Unknown type')
 
-#TODO: if sampling, the alignment is not necessary
-@perf.runningTimeDecorator
-def main(filename_subst=None):
-    
+
+def parse_arguments():
     parser = argparse.ArgumentParser(description='Realign sequence using ' + 
                                      'informations from tandem repeat finder')
     parser.add_argument('--mathType', '-m', type=str, default='float',
@@ -81,78 +125,60 @@ def main(filename_subst=None):
     parser.add_argument('--alignment_regexp', default='', 
                         help='Regular expression used to separate alignment' +
                         'in input file')
-    parser.add_argument('--sequence_regexp', nargs='+', default=["sequence1", "sequence2"],
+    parser.add_argument('--sequence_regexp', nargs='+', default=["sequence1",
+                                                                 "sequence2"],
                         help='Regular expressions used to select sequences.')
     parser.add_argument('--beam_width', default=10, type=int, 
                         help='Beam width.')
     parser.add_argument('--repeat_width', default=0, help='Maximum possible' + 
                         ' error of repeat annotation.', type=int)
     parser.add_argument('--tracks', help='Comma separated list of ' + \
-                        'annotation tracks (trf, original_repeats)', type=str,
+                        'annotation tracks (trf, original_repeats)', 
+                        type=lambda x: set(x.split(',')),
                         default='trf')
+    io_to_dict = lambda x: dict([tuple(y.split(':')) for y in x.split(',')])
     parser.add_argument('--intermediate_input_files', help='Comma separated' + 
                         'list of key value pairs: "key:value". This files ' + 
-                        'used to skip load precomputed data in algorithms.' )
+                        'used to skip load precomputed data in algorithms.',
+                        type=io_to_dict, default={})
     parser.add_argument('--intermediate_output_files', help='Comma separated' + 
                         'list of key value pairs: "key:value". This files ' + 
-                        'used to skip store precomputed data in algorithms.' )
-    parser.add_argument('--draw', default='', type=str, help='output file for image')
+                        'used to skip store precomputed data in algorithms.',
+                        type=io_to_dict, default={},)
+    parser.add_argument('--draw', default='', type=str, 
+                        help='output file for image')
     parsed_arg = parser.parse_args()
-    mathType = getMathType(parsed_arg.mathType)
-    annotations = dict()
-    tracks = set(parsed_arg.tracks.split(','))
-    io_files = dict();
-    if parsed_arg.intermediate_input_files != None:
-        io_files['input'] = dict([
-            tuple(x.split(':')) 
-            for x in parsed_arg.intermediate_input_files.split(',')
-        ])
-    else:
-        io_files['input'] = dict()
-    if parsed_arg.intermediate_output_files != None:
-        io_files['output'] = dict([
-            tuple(x.split(':')) 
-            for x in parsed_arg.intermediate_output_files.split(',')
-        ])
-    else:
-        io_files['output'] = dict()
-
+    parsed_arg.mathType = getMathType(parsed_arg.mathType)
+    
     # ====== Validate input parameters =========================================
     if len(parsed_arg.bind_file) % 2 != 0:
         sys.stderr.write('ERROR: If binding files, the number of arguments has'
                          + 'to be divisible by 2\n')
-        return 1 
+        return None 
     if len(parsed_arg.bind_constant_file) % 2 != 0:
         sys.stderr.write('ERROR: If binding constants (as files), the number of'
                          + ' arguments has to be divisible by 2\n')
-        return 1
+        return None
     if len(parsed_arg.bind_constant) % 2 != 0:
         sys.stderr.write('ERROR: If binding constants, the number of'
                          + ' arguments has to be divisible by 2\n')
-        return 1
+        return None
+    return parsed_arg
     
-    # ====== Parse parameters ==================================================
-        
-    alignment_filename_template = parsed_arg.alignment
-    output_filename_template = parsed_arg.output_file
-    
-    # ====== Load model ========================================================
-    loader = HMMLoader(mathType) # TODO: rename HMMLoader to ModelLoader
-    for i in range(0, len(parsed_arg.bind_constant), 2):
-        loader.addFile(parsed_arg.bind_file[i], parsed_arg.bind_file[i + 1])
-    for i in range(0, len(parsed_arg.bind_constant_file), 2):
-        loader.addConstant(
-            parsed_arg.bind_constant_file[i],
-            loader.load(parsed_arg.bind_constant_file[i + 1])
-        )
-    for i in range(0, len(parsed_arg.bind_constant), 2):
-        loader.addConstant(
-            parsed_arg.bind_constant[i],
-            loader.loads(parsed_arg.bind_constant[i + 1]),
-        )
-    model_filename = parsed_arg.model
-    model = loader.load(model_filename)["model"]
 
+#TODO: if sampling, the alignment is not necessary
+@perf.runningTimeDecorator
+def main(filename_subst=None):
+   
+
+    # ====== Parse parameters ==================================================
+    args = parse_arguments()
+    if args is None:
+        return 1
+        
+    alignment_filename_template = args.alignment
+    output_filename_template = args.output_file
+    
     # ====== Check SGE parameters ==============================================
     task_ids = [None]
     if os.environ.has_key('SGE_TASK_ID'):
@@ -166,6 +192,11 @@ def main(filename_subst=None):
             sge_task_id,
             min(sge_task_id + sge_step_size, sge_task_last + 1)
         )
+    
+    # ====== Load model ========================================================
+    model = get_model(args)
+
+    # ====== Realign all sequences =============================================
     for task_id in task_ids:
         if task_id == None:
             output_filename = output_filename_template
@@ -173,86 +204,57 @@ def main(filename_subst=None):
         else:
             output_filename = output_filename_template.format(id=task_id - 1)
             alignment_filename = \
-                alignment_filename_template.format(id=task_id - 1) 
+                alignment_filename_template.format(id=task_id - 1)
+         
+        annotations = compute_annotations(args, alignment_filename)
+                
         with Open(output_filename, 'w') as output_file_object:
             for aln in Fasta.load(
                 alignment_filename,
-                parsed_arg.alignment_regexp,
+                args.alignment_regexp,
                 Alignment,
-                sequence_selectors=parsed_arg.sequence_regexp
+                sequence_selectors=args.sequence_regexp
             ):
                 if len(aln.sequences) < 2:
                     sys.stderr.write("ERROR: not enough sequences in file\n")
                     exit(1)
                     
-                if len(parsed_arg.draw) == 0:
+                if len(args.draw) == 0:
                     drawer = brainwash(AlignmentCanvas)()
                 else:
                     drawer = AlignmentCanvas()
-                # Sequence 1
-                seq1 = Fasta.alnToSeq(aln.sequences[0])
-                seq1_length = len(seq1)
-                seq1_name = aln.names[0]
-                drawer.add_sequence('X', seq1)
                 
-                # Sequence 2
-                seq2 = Fasta.alnToSeq(aln.sequences[1])
-                seq2_length = len(seq2)
-                seq2_name = aln.names[1]
-                drawer.add_sequence('Y', seq2)
+                seq1, seq2 = tuple(map(Fasta.alnToSeq, aln.sequences[:2]))
                 
                 perf.msg("Data loaded in {time} seconds.")
                 perf.replace()
-                
-                #====== DRAW ORIGINAL ANNOTATION ===============================
-                drawer.add_original_alignment(aln) 
-                   
-                #====== COMPUTE ANNOTATION TRACKS ==============================
-                
-                # Compute repeat hints
-                if 'trf' in tracks:
-                    trf = None
-                    for trf_executable in parsed_arg.trf:
-                        if os.path.exists(trf_executable):
-                            trf = TRFDriver(trf_executable, mathType=mathType)
-                            break
-                    if trf:
-                        repeats = trf.run(alignment_filename)
-                        annotations['trf'] = repeats
-                                    
-                if 'original_repeats' in tracks:
-                    repeats = json.load(Open(alignment_filename + '.repeats',
-                                             'r'))
-                    for k, v in repeats.iteritems():
-                        repeats[k] = [Repeat(_v[0], _v[1], _v[2], _v[3], _v[4]) 
-                                      for _v in v]
-                    
-                    annotations['original_repeats'] = repeats
-                        
-                perf.msg("Hints computed in {time} seconds.")
-                perf.replace()
-        
-                #====== REALIGN ================================================
-                
-                realigner = getRealigner(parsed_arg.algorithm)()
-                realigner.prepareData(seq1, seq1_name, seq2, seq2_name, 
-                                      aln, parsed_arg.beam_width, drawer, model,
-                                      mathType, annotations, io_files, 
-                                      parsed_arg.repeat_width)
-                aln = realigner.realign(0, seq1_length, 0, seq2_length)
+               
+                realigner = getRealigner(args.algorithm)()
+                realigner.prepareData(seq1, aln.names[0], seq2, aln.names[1], 
+                                      aln, args.beam_width, drawer, model,
+                                      args.mathType, annotations, 
+                                      {
+                                       'input': args.intermediate_input_files,
+                                       'output': args.intermediate_output_files
+                                      }, args.repeat_width)
+                aln = realigner.realign(0, len(seq1), 0, len(seq2))
 
                 perf.msg("Sequence was realigned in {time} seconds.")
                 perf.replace()
 
-                drawer.add_alignment_line(
-                    101,
-                    (255, 0, 255, 255),
-                    2,
-                    AlignmentPositionGenerator(Alignment([aln[0], aln[2]]))
-                )
-		if len(parsed_arg.draw) > 0:
-                    drawer.draw(parsed_arg.draw, 2000, 2000)
-		    perf.msg("Image was drawn in {time} seconds.")
+           
+                if len(args.draw) > 0:
+                    drawer.add_sequence('X', seq1)
+                    drawer.add_sequence('Y', seq2)
+                    drawer.add_original_alignment(aln) 
+                    drawer.add_alignment_line(
+                        101,
+                        (255, 0, 255, 255),
+                        2,
+                        AlignmentPositionGenerator(Alignment([aln[0], aln[2]]))
+                    )
+                    drawer.draw(args.draw, 2000, 2000)
+                    perf.msg("Image was drawn in {time} seconds.")
                 
                 # Save output_file
                 Fasta.saveAlignmentPiece(aln, output_file_object)
@@ -262,4 +264,3 @@ if __name__ == "__main__":
     ret = main()
     perf.printAll()
     exit(ret)
-    
