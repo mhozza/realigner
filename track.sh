f26814be5e96e8ba12e4fc2f07ae958ac1a5690d
Michal Nánási <michal.nanasi@gmail.com>
2013-04-11 14:03:58 +0200

    Fixed tracker

--- track.sh
+++ track.sh
@@ -2,6 +2,6 @@
 
 while true; do
     #ps --format=%cpu,%mem,rss,vsz,cputime,comm,args -A |grep -v grep|grep pypy-env | sed -e 's/pypy.*$//'  | sed -e 's/^[ \t]*//' | sed -e 's/[ \t]*$//' | sed -e 's/[ \t][ \t]*/,/g';
-    ps --format=%cpu,%mem,rss,vsz,cputime,comm,args -A |grep -v grep|grep Realign.py | sed -e 's/py.*$//'  | sed -e 's/^[ \t]*//' | sed -e 's/[ \t]*$//' | sed -e 's/[ \t][ \t]*/,/g';
+    ps --format=%cpu,%mem,rss,vsz,cputime,comm,args -A |grep -v grep|grep -e '\(Expectation.py\|Realign.py\)' | sed -e 's/py.*$//'  | sed -e 's/^[ \t]*//' | sed -e 's/[ \t]*$//' | sed -e 's/[ \t][ \t]*/,/g';
     sleep 1;
 done 
