1166eb5c748fbea84f56911489c4f2cb3ea61b60
Michal Nánási <michal.nanasi@gmail.com>
2013-04-12 09:30:20 +0200

    Fixed serializing of expectation

--- bin/Expectation.py
+++ bin/Expectation.py
@@ -7,12 +7,32 @@ import sys
 from alignment.AlignmentIterator import AlignmentBeamGenerator
 import json
 from repeats.RepeatGenerator import RepeatGenerator
-from repeats.RepeatRealigner import jsonize
+from collections import defaultdict
+from algorithm.LogNum import LogNum
 
 # aj tak potrebujem pridať nejaký "realigner", lebo tam potrebujem pridat vselijake dalsie data
 # trenovatko si zoberie consenzus s hintov. Nebude to sice ciste, ale bude to funkcne.
 # Este by sa dalo zistit pravdepodobnosti a podla toho to zarovnat. To je ale grc, ale asi to tak spravim:-(
-
+def jsonize(inp):
+    t = type(inp)
+    if t == dict or t == defaultdict:
+        output = list()
+        for k, v in inp.iteritems():
+            output.append((jsonize(k), jsonize(v)))
+        return output
+    elif t == list:
+        output = []
+        for x in inp:
+            output.append(jsonize(x))
+        return output
+    elif t == type(LogNum()):
+        return inp.value
+    elif t == tuple:
+        output = []
+        for x in inp:
+            output.append(jsonize(x))
+        return tuple(output)
+    return inp
 
 def expectation_generator(args, model, alignment_filename, annotations):
     for aln in Fasta.load(
@@ -22,7 +42,7 @@ def expectation_generator(args, model, alignment_filename, annotations):
         sequence_selectors=args.sequence_regexp):
         if len(aln.sequences) < 2:
             sys.stderr.write("ERROR: not enough sequences in file\n")
-            return 1
+            raise "ERROR: not enough sequences in file"
         seq1, seq2 = tuple(map(Fasta.alnToSeq, aln.sequences[:2]))
         positionGenerator = list(
             AlignmentBeamGenerator(aln, args.beam_width)
