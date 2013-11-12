38d0a02126229993656840512080e8dea76e2a5c
Michal Nánási <michal.nanasi@gmail.com>
2013-11-12 16:02:53 +0100

    + Hight Order Repeat state

---
+++ repeats/HighOrderRepeatState.py
@@ -0,0 +1,108 @@
+from hmm.HMM import State
+from repeats.RepeatAlignmentState import PairRepeatState
+from tools.Exceptions import ParseException
+from tools.my_rand import dist_to_json
+from hmm.SpecialHMMs import createKRepeatHMM
+
+
+class HighOrderState(State):
+    def __init__(self, *p):
+        State.__init__(self, *p)
+        self.order = 0
+
+    def load(self, dictionary):
+        State.load(self, dictionary)
+        if 'order' not in dictionary:
+            raise ParseException('order was not found in state')
+        self.order = dictionary['order']
+
+    def toJSON(self):
+        ret = State.toJSON(self)
+        ret['order'] = self.order
+        return ret
+
+    def emission(self, X, x, dx):
+        wat = X[x : x + dx]
+        if x - self.order >= 0:
+            base = x - self.order
+            wat = X[base : base + dx] + wat
+        return self.emissiond[wat]
+
+
+class HighOrderRepeatState(PairRepeatState):
+
+    def __init__(self, *p):
+        PairRepeatState.__init__(self, *p)
+        self.maxK = 0
+        self.time = self.mathType(0.0)
+        self.backgroundProb = []
+        self.indelProb = self.mathType(0.0)
+        self.indelExtProb = self.mathType(0.0)
+        self.repeatProb = self.mathType(0.0)
+        self.endProb = self.mathType(0.0)
+        self.model = None
+
+    def load(self, dictionary):
+        State.load(self, dictionary)
+        if 'maxK' not in dictionary:
+            raise ParseException('maxK was not found in state')
+        self.maxK = int(dictionary['maxK'])
+        if 'time' not in dictionary:
+            raise ParseException('time was not found in state')
+        self.time = float(dictionary['time'])
+        if 'backgroundprob' not in dictionary:
+            raise ParseException('backgroundprob was not found in state')
+        self.backgroundProb = dictionary['backgroundprob']
+        if 'indelprob' not in dictionary:
+            raise ParseException('indelprob was not found in state')
+        self.indelProb = dictionary['indelprob']
+        if 'indelextprob' not in dictionary:
+            raise ParseException('indelextprob was not found in state')
+        self.indelExtProb = dictionary['indelextprob']
+        if 'repeatprob' not in dictionary:
+            raise ParseException('repeatprob was not found in state')
+        self.repeatProb = dictionary['repeatprob']
+        if 'endprob' not in dictionary:
+            raise ParseException('endprob was not found in state')
+        self.endProb = dictionary['endprob']
+        self.model = createKRepeatHMM(
+            self.mathType,
+            self.maxK,
+            self.time,
+            self.backgroundProb,
+            self.indelProb,
+            self.indelExtProb,
+            self.repeatProb,
+            self.endProb,
+        )
+
+    def toJSON(self):
+        ret = State.toJSON(self)
+        ret['maxK'] = self.maxK
+        ret['time'] = self.time
+        ret['backgroundprob'] = dist_to_json(self.backgroundProb)
+        ret['indelprob'] = self.indelProb
+        ret['indelextprob'] = self.indelExtProb
+        ret['repeatprob'] = self.repeatProb
+        ret['endprob'] = self.endProb
+        return ret
+
+    def getEmission(self, X, x, dx, tp):
+        sv = (x, dx, tp)
+        if sv in self.memoize:
+            return self.memoize[sv]
+        ret = self.model.getProbabilities(X, x, dx)
+        for ddx in range(dx + 1):
+            self.memoize[(x, ddx, tp)] = ret[ddx]
+        return ret[dx]
+
+    def emission(self, X, x, dx, Y, y, dy):
+        xp = self.getEmission(X, x, dx, 0)
+        yp = self.getEmission(Y, y, dy, 1)
+        return xp * yp
+
+    def buildSampleEmission(self):
+        return None
+
+    def sampleEmission(self):
+        return None
