7a8865383aa621d371041f4fa921bae395e4f407
Michal Hozza <mhozza@gmail.com>
2014-04-21 19:09:28 +0200

    added sequences

---
+++ data/sequences/train_bio/bio82.js
@@ -0,0 +1,1 @@
+{"__name__": "Annotations", "annotations": ["repeat"], "sequences": [{"name": "sequence1", "annotations": [{"id": "repeat", "file": "data/sequences/train_sequences/bio82_sequence1.repeat.bed"}]}, {"name": "sequence2", "annotations": [{"id": "repeat", "file": "data/sequences/train_sequences/bio82_sequence2.repeat.bed"}]}]}
