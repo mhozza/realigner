903db75dcf3c3feeac60b3d4270511741eda0222
Michal Hozza <mhozza@gmail.com>
2014-04-30 12:02:05 +0200

    generovanie bio zarovnani

--- classifier_alignment/maf_loader.py
+++ classifier_alignment/maf_loader.py
@@ -1,10 +1,10 @@
 import re
 
 
-def substring_in_keys(s, keys):
-    r = re.compile(s)
-    for k in keys:
-        if r.match(k):
+def matched(name, alignment):
+    r = re.compile(name)
+    for s in alignment:
+        if r.match(s['name']):
             return True
     return False
 
@@ -13,44 +13,63 @@ class MafLoader():
     def __init__(self, fname):
         self.fname = fname
 
+    def filter_seq(self, aln, sequences):
+        def check(s):
+            regs = map(re.compile, sequences)
+            for r in regs:
+                if r.match(s['name']):
+                    return True
+            return False
+
+        return filter(check, aln)
+
     def alignments(self):
         last_alignment = None
+        last_alignment_count = 0
         with open(self.fname, 'r') as f:
             for line in f:
                 if len(line) == 0:
                     continue
                 if line[0] == 'a':
-                    if last_alignment is not None:
-                        yield last_alignment
-                    last_alignment = dict()
+                    if last_alignment is not None and last_alignment_count > 0:
+                        yield last_alignment, last_alignment_count
+                    last_alignment = list()
+                    last_alignment_count = 0
                 if line[0] == 's':
                     l = line.split()
                     s = dict()
+                    s['name'] = l[1]
                     s['sequence'] = l[6]
                     s['offset'] = l[2]
-                    last_alignment[l[1]] = s
+                    last_alignment.append(s)
+                    last_alignment_count += 1
         if last_alignment is not None:
-            yield last_alignment
+            yield last_alignment, last_alignment_count
 
-    def filtered_alignments(self, has_sequences=None, min_length=0):
+    def filtered_alignments(self, has_sequences=None, min_length=0, remove_other=False):
         def aln_len(aln):
-            return len(aln[aln.keys()[0]]['sequence'])
-        for a in self.alignments():
+            return len(aln[0]['sequence'])
+        for a, l in self.alignments():
             if has_sequences is not None and len(has_sequences) > 0:
                 ok = True
                 for s in has_sequences:
-                    if not substring_in_keys(s, a.keys()):
+                    if not matched(s, a):
                         ok = False
                         break
                 if ok and aln_len(a) < min_length:
                     ok = False
                 if ok:
-                    yield a
+                    if remove_other:
+                        a = self.filter_seq(a, has_sequences)
+                        l = len(a)
+                    yield a, l
+
 
 if __name__ == '__main__':
     loader = MafLoader('data/sequences/bio/chr1.maf')
-    s = loader.filtered_alignments(['hg19.chr1', 'panTro2.chr1$', 'canFam2'], 1000)
+    s = loader.filtered_alignments(['hg19.chr1$', 'panTro2.chr1$', 'canFam2'], 1000, True)
     c = 0
     for i in s:
+        print i[1]
         c += 1
     print c
--- make_sequence.py
+++ make_sequence.py
@@ -4,10 +4,15 @@ import sys
 import json
 import track
 import argparse
+import pandas as pd
 from alignment import Fasta
 from classifier_alignment import simulator
+from classifier_alignment.maf_loader import MafLoader
+from functools import partial
+from collections import defaultdict
 
 seq_basename = 'sequence'
+overwrite = False
 
 
 def load_seq():
@@ -16,23 +21,50 @@ def load_seq():
     return [seq1, seq2]
 
 
-def create_fasta(fname, seq_basename, sequences):
-    names = []
-    for i, _ in enumerate(sequences):
-        names.append(seq_basename + str(i+1))
-    seq2 = [s.upper() for s in sequences]
-    Fasta.save(zip(names, seq2), fname)
+def checkfile(fname):
+    if os.path.isfile(fname):
+        if overwrite:
+            print 'removing existing file', fname
+            os.remove(fname)
+        else:
+            print 'skipping existing file', fname
+            return True
+
+def create_fasta(fname, seq_basename, sequences, rename=False):
+    if rename:
+        names = [seq_basename + str(i+1) for i in range(len(sequences))]
+    else:
+        names = [s['name'] for s in sequences]
+    assert(len(names) == len(sequences))
+    seq = [s['sequence'].upper() for s in sequences]
+    Fasta.save(zip(names, seq), fname)
     return names
 
 
-def cerate_repeat_annotation(fname, sequence, name):
+def create_repeat_annotation(sequence, fname):
+    if checkfile(fname):
+        return
     annotation = (x.isupper() for x in sequence)
     intervals = simulator.sequence_to_intervals(
-        simulator.get_sequence(annotation, sequence), name
+        simulator.get_sequence(annotation, sequence), 'repeat'
     )
     with track.new(fname, 'bed') as t:
         t.fields = ['start', 'end', 'name']
         t.write("chr1", intervals)
+
+
+def create_gene_annotation(source_fname, chromosome, fname):
+    if checkfile:
+        return
+    genes = pd.read_csv(source_fname, sep='\t', header=None)
+    if type(genes[0][0]) is int:
+        indices = [2, 4, 5]
+    else:
+        indices = [1, 3, 4]
+    genes = genes[genes[indices[0]] == chromosome]
+    genes['desc'] = 'gene'
+    genes = genes[indices + ['desc']]
+    genes.to_csv(fname, sep='\t', header=['track type="bed"', 'converted_by="mio"', '', ''])
 
 
 def create_config(fname, annotations, names, annotation_files):
@@ -50,45 +82,67 @@ def create_config(fname, annotations, names, annotation_files):
     f.close()
 
 
-def add_annotation(
-        sequences,
-        annotations,
-        annotation_files,
-        name,
-        filename_template,
-        func,
-        args,
+def make_annotation_fname(
+    data_dir,
+    base_fname,
+    sequence_name,
+    annotation_name,
+    template='{basename}_{sequence}.{annotation}.bed'
 ):
-    annotations.append(name)
-    for seq_name, seq in sequences:
-        filename = filename_template.format(seq_name)
-        if not seq_name in annotation_files:
-            annotation_files[seq_name] = list()
-        annotation_files[seq_name].append({'id': name, 'file': filename})
-        func(filename, seq, seq_name, *args)
+    return os.path.join(
+        data_dir,
+        template.format(basename=base_fname, annotation=annotation_name, sequence=sequence_name),
+    )
 
 
-def main(sequences, base_fname='bio', data_dir='data/bio_seq'):
+def extract_seq_info(seq_name):
+    org, chrom = seq_name.split('.')
+    return org, chrom
+
+
+def make_alignment(sequences, rename=False, base_fname='bio', data_dir='data/sequences/bio'):
+    def add_annotation(seq_name, name, fname_func, ann_func, offset=0):
+        filename = fname_func(name)
+        annotation_files[seq_name].append({'id': name, 'file': filename, 'offset': offset})
+        ann_func(filename)
+
     seq_fname = os.path.join(data_dir, base_fname + '.fa')
     config_fname = os.path.join(data_dir, base_fname + '.js')
-    annotations = list()
-    annotation_files = dict()
-    names = create_fasta(seq_fname, seq_basename, sequences)
-    add_annotation(
-        zip(names, sequences),
-        annotations,
-        annotation_files,
-        'repeat',
-        os.path.join(data_dir, base_fname + '_{}.repeat.bed'),
-        cerate_repeat_annotation,
-        [],
-    )
+    annotations = ['gene', 'repeat']
+    annotation_files = defaultdict(list)
+    names = create_fasta(seq_fname, seq_basename, sequences, rename)
+    for seq_name, s in zip(names, sequences):
+        make_fname = partial(make_annotation_fname, data_dir, base_fname, s['name'])
+        org, chrom = extract_seq_info(s['name'])
+        repeat = partial(create_repeat_annotation, s['sequence'])
+        add_annotation(seq_name, 'repeat', make_fname, repeat)
+        gene = partial(create_gene_annotation, 'data/sequences/bio/genscan.{}.txt'.format(org), chrom)
+        add_annotation(seq_name, 'gene', make_fname, gene, offset=s['offset'])
     create_config(config_fname, annotations, names, annotation_files)
 
 
+def make_from_maf(
+    fname,
+    sequences,
+    min_size=0,
+    limit=None,
+    base_fname='bio',
+    data_dir='data/sequences/bio'
+):
+    loader = MafLoader(fname)
+    for i, (alignment, _) in enumerate(loader.filtered_alignments(sequences, min_size, remove_other=True)):
+        if limit is not None:
+            if limit == 0:
+                break
+            limit -= 1
+        print 'make_alignment', i
+        make_alignment(alignment, base_fname='bio'+str(i), rename=True)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('name', metavar='name', type=str)
-    args = parser.parse_args()
-    sequences = load_seq()
-    main(sequences, args.name)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('name', metavar='name', type=str)
+    # args = parser.parse_args()
+    # sequences = load_seq()
+    # main(sequences, args.name)
+    make_from_maf('data/sequences/bio/chr1.maf', ['hg19', 'panTro2.chr1$', 'canFam2'], 1000)
