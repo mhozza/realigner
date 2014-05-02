a832e4cf19d1528e5f44de953dd6102f7cd38eb6
Michal Hozza <mhozza@gmail.com>
2014-05-02 20:56:51 +0200

    muscle s tranzitivitou a multi

--- run_muscle.py
+++ run_muscle.py
@@ -17,21 +17,47 @@ def select_sequences(inp_filename, out_filename, sequences):
 
 
 def main(base_fname, base_dir):
-    inp_filename = os.path.join(base_dir, base_fname + '.fa')
-    tmp_filename = os.path.join(base_dir, base_fname + '.selected.fa')
-    out_filename = os.path.join(base_dir, base_fname + '.muscle.fa')
-    sequences = ['sequence1', 'sequence2']
-    select_sequences(inp_filename, tmp_filename, sequences)
-    os.system("muscle -in {inp} -out {out} 2> /dev/null".format(
-        inp=tmp_filename,
-        out=out_filename,
-    ))
-    print 'Equality: ' + str(transitivitycheck.compare_with_source(inp_filename, out_filename, sequences[0], sequences[1]))
-    os.remove(tmp_filename)
+    src = os.path.join(base_dir, base_fname + '.fa')
+    tmpf = os.path.join(base_dir, 'realigned/' + base_fname + '.selected.fa')
+    dst = os.path.join(
+        base_dir, 'realigned/' + os.path.splitext(base_fname)[0] + '.{}_{}.muscle.fa'
+    )
+    sequences = ['sequence1', 'sequence2', 'sequence3']
+
+    for x in range(len(sequences)-1):
+        for y in range(x+1, len(sequences)):
+            sX = sequences[x]
+            sY = sequences[y]
+            if sX != sY:
+                select_sequences(src, tmpf, [sequences[x], sequences[y]])
+                dest = dst.format(sX, sY)
+                os.system("muscle -in {inp} -out {out} 2> /dev/null".format(
+                    inp=tmpf,
+                    out=dest,
+                ))
+    if len(sequences) == 3:
+        print 'Transitivity: ' + str(transitivitycheck.score(dst))
+    sX = sequences[0]
+    sY = sequences[1]
+    print 'Equality: ' + str(
+        transitivitycheck.compare_with_source(src, dst.format(sX, sY), sX, sY)
+    )
+    os.remove(tmpf)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('name', metavar='name', type=str, default='simulated_alignment', nargs='?')
-    parser.add_argument('dir', metavar='dir', type=str, default='data/sequences/simulated', nargs='?')
+    parser.add_argument(
+        'dir', metavar='dir', type=str, default='data/sequences/simulated', nargs='?'
+    )
+    parser.add_argument('--multi', action="store_true")
     args = parser.parse_args()
-    main(args.name, args.dir)
+    if args.multi:
+        sequences = list()
+        for fn in filter(
+            lambda x: path.splitext(x)[1] == '.fa' and args.name in path.splitext(x)[0], listdir(args.dir)
+        ):
+            if path.isfile(path.join(args.dir, fn)):
+                main(fn, args.dir)
+    else:
+        main(args.name+'.fa', args.dir)
